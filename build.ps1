f36e0d81c479df8884fee4169b809ea16f45cb53
Jeff Sharp <public@sharpjs.net>
2021-01-27 15:21:17 -0800

    v2.225

--- build.ps1
+++ build.ps1
@@ -38,7 +38,7 @@ Set-StrictMode -Version Latest
 
 # Run remainder of build in an ephemeral Docker container
 & docker run -t --rm        `
-    -e VERSION=2.221        `
+    -e VERSION=2.225        `
     -v ${PSScriptRoot}:/src `
     python:alpine           `
     /src/build.sh
